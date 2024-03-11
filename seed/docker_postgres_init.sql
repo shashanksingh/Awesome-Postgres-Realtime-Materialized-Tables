8a071a8238f92c11b2d00b2975ae42fe2b222621
shashank singh <shashank.personal@gmail.com>
2024-03-11 11:59:01 +0000

    added dockerfile for postgres

---
+++ seed/docker_postgres_init.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA IF NOT EXISTS analytics;
+-- TIP : Passwords would be encrypted and stored in key management services like AWS KMS and not in code base
+CREATE USER event_ingestion WITH encrypted password 'password';
+
+GRANT ALL PRIVILEGES ON schema analytics TO event_ingestion;
+
+CREATE TABLE IF NOT EXISTS analytics.events
+(
+    id uuid DEFAULT gen_random_uuid(),
+    event_name varchar(512),
+    data JSONB,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT url_primary_key PRIMARY KEY (id)
+) ;
+
+
+CREATE EXTENSION pg_ivm;
+select * from create_immv(
+  'analytics.event_count',
+  'select count(*) from analytics.events'
+);
+ALTER TABLE analytics.event_count REPLICA IDENTITY DEFAULT;
+
+
+-- dummy data
+insert into analytics.events (event_name, data) values("CUSTOMER_VIEW", '{"title": "My first day at work", "Feeling": "Mixed feeling"}');
